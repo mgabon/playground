65074a1118b71845e1939df5ed9893eabbafb596
Mark Gabon <markgabon@hotmail.com>
2022-03-27 11:14:04 -0400

    initial commit

---
+++ Desktop/UnitFour/playground/src/App.js
@@ -0,0 +1,34 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">  
+
+      <header className="top">
+    <h1>Hi i'm Mark Gabon!</h1>
+     <h2>I build things for the web.</h2>
+      </header>
+
+      <div className="about">
+    <h2>About Me:</h2>
+    <p>My name is Mark and I enjoy creating things on the internet. My interest in web development started back in 2012 when I was customising my profile page on a game. I've always been intruiged on how things worked and learning how to code excited me.</p>
+
+    <div className='list'>
+      <p>Heres a few skills/technologies I learned throughout my experience:</p>
+  <ul>
+    <li>JavaScript</li>
+    <li>React</li>
+    <li>CSS</li>
+    <li>Node.js</li>
+  </ul>
+  </div>
+  </div>
+<div className='projects'>
+  <h2>Projects:</h2>
+</div>
+    </div>
+  );
+}
+
+export default App;
