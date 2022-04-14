5b184699e4e1898d5ed04283f14a77c2fdd499d6
Mark Gabon <markgabon@hotmail.com>
2022-04-14 13:09:58 -0400

    Added more sections/divs for profile

--- Desktop/UnitFour/playground/src/App.js
+++ Desktop/UnitFour/playground/src/App.js
@@ -3,31 +3,33 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">  
-
-      <header className="top">
-    <h1>Hi i'm Mark Gabon!</h1>
-     <h2>I build things for the web.</h2>
-      </header>
-
-      <div className="about">
-    <h2>About Me:</h2>
-    <p>My name is Mark and I enjoy creating things on the internet. My interest in web development started back in 2012 when I was customising my profile page on a game. I've always been intruiged on how things worked and learning how to code excited me.</p>
-
-    <div className='list'>
-      <p>Heres a few skills/technologies I learned throughout my experience:</p>
-  <ul>
-    <li>JavaScript</li>
-    <li>React</li>
-    <li>CSS</li>
-    <li>Node.js</li>
-  </ul>
-  </div>
-  </div>
-<div className='projects'>
-  <h2>Projects:</h2>
-</div>
+    <div className="App">
+<header>
+    <div className='navbar'>
+    <a href="#">GitHub</a>
+    <a href="#">LinkedIn</a>
+    <a href="#">Email</a>
     </div>
+</header>
+
+<div>
+  <section>
+    <div className='profile-greeting'>
+      <h1>Hello Traveler, <br/> I'm Mark.</h1>
+    </div>
+  </section>
+  <section>
+    <div className='profile'>
+    <div className='profile-bio'>
+    <h2>About me</h2>
+    <p>test</p>
+    </div>
+    </div>
+  </section>
+</div>
+
+
+</div>
   );
 }
 
